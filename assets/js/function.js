83c3edfbfd3c0cbfc81fb795a45f43e44dd3408a
admin <gregorio500@ymail.com>
2017-03-15 15:23:53 +0000

    before gummy slider

--- assets/js/function.js
+++ assets/js/function.js
@@ -2,23 +2,30 @@ $( window ).on( "load", function() {
 
 console.log( "ready!22222" );
 
-function setActive(li){
-	var clickedIndex = li.index();
-	
-	$(".fp-slidesNav .active").removeClass("active");
-	li.addClass("active");
-	
-	$(".slide-base > div").eq(clickedIndex).animate{ scrollTop: $(this).offset().top
-            }, 2000);
+
 
 
 	
-$(".fp-slidesNav > li").on("click", function() {
-	setActive($(this));
+$(".slider-nav > div").on("click", function(event) {
+	
+	/*setActive($(this));*/
+	var index = $(this).index(); 
+	var domElement = $(".slide-base .slide").eq(index);
+	
+	/*$(".slide-base .active").removeClass("active");*/
+	domElement.addClass("active");
+	$("html, body").animate({
+		scrollTop: domElement.offset().top }, 2500);
+	});
+
+
+
+
+
 });
 
 
 
-});
+
 
 
