006a8402dcee75b8c198ac2261fc924facf428a5
admin <gregorio500@ymail.com>
2017-03-30 13:23:34 +0100

    scale svg fib

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -24,28 +24,20 @@ var $logo = $(".logo-gt");
 //});
 
 
-$('.logo-gt').on('mouseover', function(){
+$('.logo-gt').on('mouseenter', function(){
 	
 	function overHandler(e) {
 		var rotation = 0;
 		var rotation2 = 720;
-		var tween1 = true;
-		var tween2 = true;
 		var tl = new TimelineMax();	
 		
+		if(TweenMax.isTweening($logo)) {
+			return; } else {
 		tl.set($logo, {scale: 1, rotation: 0});
-		console.log("primary " + tween1);
-		if(tl.isActive() === true) {
-			console.log("if it is false "+ tween1);
-			return tl.resume();
-		} else {
-		tween1 = tl.to($logo, 2, {scale: 0.7, rotation: rotation + 720 });
-		/*tl.set($logo, {rotation: 0});*/
-		tween2 = tl.to($logo, 3, {scale: 1, rotation: rotation2 + 360});
-		/*tl.set($logo, {scale: 1, rotation: 0});*/ 
-			console.log("after else" + tween1);
+		tl.to($logo, 0.8, {scale: 0.5, rotation: rotation + 720, ease: SlowMo.ease.config(0.7, 0.4) });
+		tl.to($logo, 1.2, {scale: 1, rotation: "+=360", ease: Elastic.easeOut.config(0.5, 0.3) });
+			};
 		
-		}
 };
 	overHandler();
 	return false;
