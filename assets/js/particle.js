85a271b32ffe5529907740c868e49df748f80687
admin <gregorio500@ymail.com>
2017-03-31 14:18:03 +0100

    svg is ready to animation

--- assets/js/particle.js
+++ assets/js/particle.js
@@ -1,143 +1,26 @@
-var paths = document.getElementsByClassName("path");
-    points = [];//holds our series of x/y values for anchors and control points
 
 
-for(var i = 0; i<paths.length; i++) {
-  points[i] = [];
-  
-  var data = Snap.path.toCubic(paths[i].getAttribute('d'))
-      dataLength = data.length,
-      pointsString = data.toString();
+var snapA = Snap("#svgA");
 
-  // convert cubic data to GSAP bezier
-  for (var j = 0; j < dataLength; j++) {
-    var seg = data[j];
-    if (seg[0] === "M") { // move (starts the path)
-      var point = {};
-      point.x = seg[1];
-      point.y = seg[2];
-      points[i].push(point);
-    } else { // seg[0] === "C" (Snap.path.toCubic should return only curves after first point)
-      for (var k = 1; k < 6; k+=2) {
-        var point = {};
-        point.x = seg[k];
-        point.y = seg[k+1];
-        points[i].push(point);
-      }
-    }
-  }  
-}
+var pathA = snapA.path("M738.5,249.5c45.01,0,81.5-36.49,81.5-81.5 M657,168c0,45.01,36.49,81.5,81.5,81.5 M820,5c-90.02,0-163,72.98-163,163 M1064.5,249.5C1064.5,114.47,955.03,5,820,5 M657,657c225.06,0,407.5-182.44,407.5-407.5 M5,5c0,360.09,291.91,652,652,652").attr({
+	id: "fibanocci",
+	fill: "none",
+	strokeWidth: "4",
+	stroke: "#419ccc",
+	strokeMiterlimit: "10"
+});
 
-var emitters = document.getElementsByClassName("emitter");
 
-var emitter1 = {
-    emitterSize: 45,
-    dotPool: [],
-    dotIndex: 0,
-    dotQuantity: 20,
-    dotSizeMax: 8,
-    dotSizeMin: 1,
-    speed: -1,
-    gravity: 0
-};
+var animatePath = pathA.getTotalLength();
 
-var carrot = {
-    emitterSize: 45,
-    dotPool: [],
-    dotIndex: 0,
-    dotQuantity: 20,
-    dotSizeMax: 8,
-    dotSizeMin: 1,
-    speed: -1,
-    gravity: 0
-};
 
-var emitterObjs = [ emitter1, carrot ];
 
-var tweens = [];
-
-// Set each path
-for(var i=0; i<emitters.length; i++) {  
-  TweenLite.set(emitters[i], {x:points[i][0].x, y:points[i][0].y})
-  //this tween calls the hideDots function when it is done. All dots with class ".emitter"+i will be hidden
-  //start both tweens paused
-  var tween = TweenMax.to(emitters[i], 8, {bezier:{type:"cubic", values:points[i]}, force3D:true, ease:Power0.easeNone, onComplete:hideDots, onCompleteParams:[".emitter" + i], paused:true, align:".emitter"});
-  
-  tweens.push(tween);
-  
-  TweenLite.set(emitterObjs[i], { width:emitterObjs[i].emitterSize, height:emitterObjs[i].emitterSize, xPercent:-50, yPercent:-50 });
-  
-  for (var j = emitterObjs[i].dotQuantity - 1; j >= 0; j--) {
-    dot = document.createElement("div");
-    //give each dot a class based on which emitter it belongs to "dot emitter0" or "dot emitter1"
-    dot.className = "dot emitter" + i;
-    TweenLite.set(dot, { xPercent:-50, yPercent:-50, force3D:true });
-    document.body.appendChild(dot);
-    emitterObjs[i].dotPool[j] = dot;
-  }
-  
-  var explosion = new TimelineMax({ repeat: -1 }).call(shootDot, [[emitters[i], emitterObjs[i]]], null, 2 / emitterObjs[i].dotQuantity);
-}
-
-$(".startPath").click(function(){
-  tweens[0].restart();
-  showDots(".emitter0")
-})
-
-$(".startPath2").click(function(){
-  tweens[1].restart();
-  showDots(".emitter1")
-})
-
-function shootDot(emit) {
-  var elem = emit[0],
-      emitter = emit[1];
-    
-  var angle, length, dot, size, bounds = elem.getBoundingClientRect();
-
-  //create all the dots
-    
-  dot = emitter.dotPool[emitter.dotIndex++];
-  if (emitter.dotIndex === emitter.dotQuantity) emitter.dotIndex = 0;
-  size = getRandom(emitter.dotSizeMin, emitter.dotSizeMax);
-  angle = Math.random() * Math.PI * 2; //random angle
-  //figure out the maximum distance from the center, factoring in the size of the dot (it must never go outside the circle), and then pick a random spot along that length where we'll plot the point. 
-  length = Math.random() * (emitter.emitterSize / 2 - size / 2); 
-  //place the dot at a random spot within the emitter, and set its size.
-
-  TweenLite.set(dot, {
-    opacity:1,
-    x:Math.cos(angle) * length + bounds.left + bounds.width / 2,
-    y:Math.sin(angle) * length + bounds.top + bounds.height / 2,
-    width:size,
-    height:size
+var emitter = snapB.circle(26,26,16);
+  CircleB.attr({
+    fill: "#f00959",
+    stroke: "#fff",
+    strokeWidth: 2
   });
-  //this is where we do the animation...
-  TweenLite.to(dot, 1 + Math.random(), {
-    opacity:0,
-    //if you'd rather not do physics, you could just animate out directly by using the following 2 lines instead of the physics2D:
-    //x:Math.cos(angle) * length * 6, 
-    //y:Math.sin(angle) * length * 6
-  }, 0);
-}
-
-
-function getRandom(min, max) {
-  return min + Math.random() * (max - min);
-}
-
-
-function hideDots(className) {
-  TweenLite.set(className,  {visibility:"hidden"})
-}
-
-
-function showDots(className) {
-  TweenLite.set(className,  {visibility:"visible"})
-}
-
-hideDots(".emitter0, .emitter1");
-
 
 
 	
